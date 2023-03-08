101c29e405da6b1ff94579dda3f354c95b4df9b3
ega91 <44427684+ega91@users.noreply.github.com>
2023-03-08 15:22:20 +0700

    Mail templated words
    Mail templated added with default words

--- mailTemplate.php
+++ mailTemplate.php
@@ -1,2 +1,53 @@
 <div class="content">
+    <span class="preheader">This is preheader text. Some clients will show this text as a preview.</span>
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body">
+        <tr>
+            <td>&nbsp;</td>
+            <td class="container">
+                <div class="content">
+                    
+                <!-- START TABLE TAQ HERE -->
+                <table role="presentation" class="main">
+
+                <!-- START MAIN CONTENT AREA -->
+                <tr>
+                    <td class="wrapper">
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                        <tr>
+                        <td>
+                            <h1>Email Verification</h1>
+                        <p style="top:14px;">Dear [User],</p>
+                            <p>Your email [user]@gmail.com needs verification.<br/>
+                                Please enter this code to verify your email</p>
+                                <p style="padding-bottom:32px;"></p>
+                            <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-mike">
+                            <tbody>
+                                <tr>
+                                <td align="center">
+                                    <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="padding-bottom:32px;">
+                                    <tbody>
+                                        <tr>
+                                            <!-- <td><a href="[verification link]" class="button">Verify Email</a></td> -->
+                                            <td> <a href="#" target="_blank" class="btn">Verify Email</a> </td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+                                </td>
+                                </tr>
+                            </tbody>
+                            </table>
+                            <p>Regards,<br/>
+                            Indonesia MIKE Award</p>
+                        </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+                <!-- END MAIN CONTENT AREA -->
+            </table>
+        </div>
+    </td>
+    <td>&nbsp;</td>
+</tr>
+</table>
 </div>
